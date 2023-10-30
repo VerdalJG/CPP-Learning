1dad7757d460683706bd2b4ca8711f0df54decd5
Vicente Cupello <88113293+VerdalJG@users.noreply.github.com>
2023-10-30 15:37:09 +0100

    Copy files

---
+++ Practica9/FileUtils.cpp
@@ -0,0 +1,59 @@
+#include "FileUtils.h"
+#include <cstdio>
+
+namespace FileUtils
+{
+	// Mode is read/write etc
+	bool CFile::OpenFile(const char* path, const char* mode)
+	{
+		CloseFile();
+
+		std::FILE* pFile;
+		pFile = fopen(path, mode);
+
+		if (pFile)
+		{
+			m_pFile = pFile;
+			return true;
+		}
+		return false;
+		
+	}
+
+
+	bool CFile::CloseFile()
+	{
+		if (m_pFile)
+		{
+			std::FILE* pFile = reinterpret_cast<std::FILE*>(m_pFile);
+			bool bResult = fclose(pFile) == 0;
+			pFile = nullptr;
+			return bResult;
+		}
+		return false;
+	}
+
+
+	unsigned int CFile::ReadFile(char* _pBuffer, unsigned int _uBufferSize)
+	{
+		if (m_pFile)
+		{
+			std::FILE* pFile = reinterpret_cast<std::FILE*>(m_pFile);
+			unsigned int uResult = static_cast<unsigned int>(fread(_pBuffer, sizeof(char), _uBufferSize, pFile));
+			return uResult;
+		}
+		return 0;
+	}
+
+
+	unsigned int CFile::WriteFile(char* _sToWrite, unsigned int _uBufferSize)
+	{
+		if (m_pFile)
+		{
+			std::FILE* pFile = reinterpret_cast<std::FILE*>(m_pFile);
+			unsigned int uResult = static_cast<unsigned int>(fwrite(_sToWrite, sizeof(char), _uBufferSize, pFile));
+			return uResult;
+		}
+		return 0;
+	}
+}
