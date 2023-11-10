4045ef55281469b4e424abc5fc8d33d2d475f6a5
Vicente Cupello <88113293+VerdalJG@users.noreply.github.com>
2023-11-10 15:53:19 +0100

    Practica 13 done

--- Practica13/Practica13.cpp
+++ Practica13/Practica13.cpp
@@ -2,18 +2,29 @@
 
 class Image
 {
-protected:
-	int m_resolutionX, m_resolutionY;
-	int m_colorDepth;
-	int r, g, b = 0;
+public:
+	int m_resolutionX, m_resolutionY = 0;
+	int m_colorDepth = 0;
 	int DrawRGBImage(unsigned char* buffer)
 	{
 		return 0;
 	}
+	enum class ImageType
+	{
+		Invalid,
+		JPG,
+		PNG
+	};
+	ImageType type = ImageType::Invalid;
 };
 
 class JPG : public Image
 {
+public:
+	JPG()
+	{
+		type = ImageType::JPG;
+	}
 	//CCodecPNG m_Codec;
 	int DrawPNGImage(unsigned char* buffer)
 	{
@@ -23,28 +34,66 @@ class JPG : public Image
 
 class PNG : public Image
 {
-	PNG(int r, int g, int b, int a)
-		: r(r)
-		, g(g)
-		, b(b)
-		, a(a)
+public:
+	PNG()
+	:	m_alpha(0)
 	{
-
+		type = ImageType::PNG;
 	}
-	int a = 0;
+
+
 	//CCodecPNG m_Codec;
 	int DrawJPGImage(unsigned char* buffer)
 	{
 		return 0;
 	}
 
-	int RemoveAlphaChannel(unsigned char* buffer)
+	bool RemoveAlphaChannel()
 	{
-
+		m_alpha = 0;
+		printf("Alpha removed\n");
+		return m_alpha == 0;
 	}
+
+private:
+	unsigned short m_alpha;
 };
+
+void RemoveAlphaChannels(Image* imagePtr, int tableLength);
 
 int main()
 {
-	
+	PNG png;
+	png.RemoveAlphaChannel();
+
+	int tableLength = 10;
+	Image imageTable[10]
+	{
+		PNG(),
+		Image(),
+		JPG(),
+		Image(),
+		JPG(),
+		JPG(),
+		Image(),
+		JPG(),
+		PNG(),
+		Image()
+	};
+
+	Image* imageTablePtr = imageTable;
+	RemoveAlphaChannels(imageTablePtr, tableLength);
+}
+
+void RemoveAlphaChannels(Image* imagePtr, int tableLength)
+{
+	PNG* pngPtr = static_cast<PNG*>(imagePtr);
+	for (int i = 0; i < tableLength; i++)
+	{
+		if ((pngPtr + i)->type == PNG::ImageType::PNG)
+		{
+			(pngPtr + i)->RemoveAlphaChannel();
+		}
+		
+	}
 }
