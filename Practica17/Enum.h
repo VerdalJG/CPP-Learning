8434a99d1ed25cebf4eaa813d4d40748a3d787d0
Vicente Cupello <88113293+VerdalJG@users.noreply.github.com>
2023-11-23 18:25:34 +0100

    Prac 17 Completed

--- Practica17/Enum.h
+++ Practica17/Enum.h
@@ -4,15 +4,15 @@
     _VALUE,
 
 #define DECLARE_ENUM_TO_STRING(_VALUE) \
-    if(_enum == _VALUE) return #_VALUE;
+    if(_enum == Enum::_VALUE) return #_VALUE;
 
 #define DECLARE_STRING_TO_ENUM(_VALUE) \
-    if(strcmp(#_VALUE, _string) == 0) return _VALUE;
+    if(strcmp(#_VALUE, _string) == 0) return Enum::_VALUE;
 
 #define DECLARE_ENUM(_NAME, _VALUES) \
 struct _NAME \
 { \
-    enum Enum \
+    enum class Enum \
     { \
         _VALUES(DECLARE_ENUM_VALUE) \
         Count, \
@@ -26,6 +26,6 @@ struct _NAME \
     static Enum ToEnum(const char* _string) \
     { \
         _VALUES(DECLARE_STRING_TO_ENUM) \
-        return Invalid; \
+        return Enum::Invalid; \
     } \
 };
--- Practica17/Practica17.cpp
+++ Practica17/Practica17.cpp
@@ -12,11 +12,11 @@ DECLARE_ENUM(EColor, ENUM_COLORS)
 
 int main()
 {
-    EColor::Enum test1 = EColor::Red;
-    EColor::Enum test2 = EColor::Orange;
-    EColor::Enum test3 = EColor::Yellow;
+    EColor::Enum test1 = EColor::Enum::Red;
+    EColor::Enum test2 = EColor::Enum::Orange;
+    EColor::Enum test3 = EColor::Enum::Yellow;
 
-    printf("There are %d entries in enum\n", static_cast<int>(EColor::Count));
+    printf("There are %d entries in enum\n", static_cast<int>(EColor::Enum::Count));
     printf("The variable test1 is: %s\n", EColor::ToString(test1));
     printf("The variable test2 is: %s\n", EColor::ToString(test2));
     printf("The variable test3 is: %s\n", EColor::ToString(test3));
