391a880bd5ba6808927af9e1be3e02e528d875b9
Vicente Cupello <88113293+VerdalJG@users.noreply.github.com>
2023-11-26 18:23:08 +0100

    Fixed Templates .h file

--- Practica19/List.h
+++ Practica19/List.h
@@ -5,9 +5,34 @@ class TListNode
 {
 public:
 	// Constructors
-	TListNode(T* _item);
-	TListNode(T _item);
-	TListNode(const TListNode& _tListNode);
+	TListNode(T* _item) : m_pItem(_item) {}
+	
+
+	TListNode(T _item)
+	{
+		m_pItem = new T;
+		*m_pItem = _item;
+	}
+
+	TListNode(const TListNode& _tListNode)
+	{
+		m_pNext = nullptr;
+		m_pItem = nullptr;
+
+		if (_tListNode.m_pItem)
+		{
+			m_pItem = new T;
+			*m_pItem = *(_tListNode.m_pItem);
+		}
+		else
+		{
+			m_pItem = nullptr;
+		}
+
+		//printf("Node Copy Constructor\n");
+	}
+
+
 	~TListNode();
 
 
@@ -16,29 +41,153 @@ public:
 	T* m_pItem = nullptr;
 };
 
-template<typename T>
+template< class T >
 class List
 {
 public:
-	List();
+	List()
+		: m_uSize(0)
+		, m_pFirst(nullptr)
+		, m_pCurrent(nullptr)
+	{
 
-	List(const List& _list);
+	}
 
-	unsigned int Size(); // Returns the number of elements in the list
+	List(const List& _list)
+		: m_uSize(_list.m_uSize)
+		, m_pFirst(nullptr)
+		, m_pCurrent(nullptr)
+	{
+		if (_list.m_pFirst)
+		{
+			m_pFirst = new TListNode<T>(_list.m_pFirst->m_pItem);
+			TListNode<T>* currentNode = m_pFirst;
+			TListNode<T>* sourceNode = _list.m_pFirst->m_pNext;
 
-	int Push(T* item); // Adds an element to the list, placing it first
+			while (sourceNode)
+			{
+				// Create a new node and copy the contents of the source node
+				currentNode->m_pNext = new TListNode<T>(*(sourceNode));
+				currentNode = currentNode->m_pNext;
+				sourceNode = sourceNode->m_pNext;
+			}
+		}
+		else
+		{
+			m_pFirst = nullptr;
+		}
+		m_pCurrent = m_pFirst;
 
-	int Push(T item); // Adds an element to the list, placing it first
+		printf("List Copy Constructor\n");
+	}
 
-	T* First(); // Returns the first element from the list
+	unsigned int Size() // Returns the number of elements in the list
+	{
+		return m_uSize;
+	}
 
-	T* Next(); // Returns the next element from the list
+	int Push(T* item) // Adds an element to the list, placing it first
+	{
+		if (m_pFirst == nullptr)
+		{
+			m_pFirst = new TListNode<T>(item);
+			m_pCurrent = m_pFirst;
+		}
+		else
+		{
+			TListNode<T>* pList = m_pFirst; // pointer to first
+			while (pList->m_pNext) // if the first item has a next item
+			{
+				pList = pList->m_pNext; // set the pointer to the next, until you reach the last item
+			}
+			pList->m_pNext = new TListNode<T>(item); // once you have the last item, create a new object and set next to its pointer
+		}
+		return ++m_uSize;
+	}
 
-	T* Pop(); // Eliminates 1st element from the list and returns it
+	int Push(T item) // Adds an element to the list, placing it first
+	{
+		if (m_pFirst == nullptr)
+		{
+			m_pFirst = new TListNode<T>(item);
+			m_pCurrent = m_pFirst;
+		}
+		else
+		{
+			TListNode<T>* pList = m_pFirst; // pointer to first
+			while (pList->m_pNext) // if the first item has a next item
+			{
+				pList = pList->m_pNext; // set the pointer to the next, until you reach the last item
+			}
+			pList->m_pNext = new TListNode<T>(item); // once you have the last item, create a new object and set next to its pointer
+		}
+		return ++m_uSize;
+	}
 
-	void Reset(); // Eliminate all list elements
+	T* First() // Returns the first element from the list
+	{
+		m_pCurrent = m_pFirst;
 
-	void GetReverseList(); // Returns a new list with reversed elements
+		// Return the first value if it exists
+		if (m_pFirst)
+		{
+			return m_pFirst->m_pItem;
+		}
+
+		// Return nullptr if it doesnt
+		return nullptr;
+	}
+
+	T* Next() // Returns the next element from the list
+	{
+		if (m_pCurrent != nullptr)
+		{
+			m_pCurrent = m_pCurrent->m_pNext;
+		}
+		else
+		{
+			if (m_pFirst) m_pCurrent = m_pFirst;
+		}
+		return m_pCurrent ? m_pCurrent->m_pItem : nullptr;
+	}
+
+	T* Pop() // Eliminates 1st element from the list and returns it
+	{
+		T* result = nullptr;
+		if (m_pFirst)
+		{
+			TListNode<T>* pTmp = m_pFirst;
+			result = m_pFirst->m_pItem;
+			m_pFirst = m_pFirst->m_pNext;
+			delete pTmp;
+			--m_uSize;
+		}
+		return result;
+	}
+
+	void Reset() // Eliminate all list elements
+	{
+		while (m_pFirst)
+		{
+			Pop();
+		}
+	}
+
+	void GetReverseList() // Returns a new list with reversed elements
+	{
+		TListNode<T>* previous = nullptr;
+		TListNode<T>* current = m_pFirst;
+		TListNode<T>* next = nullptr;
+
+		while (current != nullptr)
+		{
+			next = current->m_pNext;
+			current->m_pNext = previous;
+			previous = current;
+			current = next;
+		}
+		m_pFirst = previous;
+	}
 
 
 
--- Practica19/Practica19.cpp
+++ Practica19/Practica19.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "List.h"
-#include "List.cpp"
 
 int main()
 {
